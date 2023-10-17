b5f15d4a484744477a0391e8f1280c07871195fa
Emma Bjerknes <Bjerknes.Emma@gmail.com>
2023-10-17 14:52:13 +0200

    first commit

---
+++ app/addDocument/page.tsx
@@ -0,0 +1,8 @@
+//new url for adding a document
+export default function addDocument() {
+  return (
+    <>
+      <h2>Add</h2>
+    </>
+  );
+}
---
+++ app/components/Main.tsx
@@ -0,0 +1,7 @@
+export default function Main({ children }: { children: React.ReactNode }) {
+  return (
+    <main>
+      <div>{children}</div>
+    </main>
+  );
+}
