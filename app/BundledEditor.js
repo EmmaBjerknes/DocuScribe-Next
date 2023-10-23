8ea1a5f9cc2ce1c679ed4bc8c347f2f7a82f712d
Emma Bjerknes <Bjerknes.Emma@gmail.com>
2023-10-23 16:04:24 +0200

    fixed navigator

--- app/BundledEditor.js
+++ app/BundledEditor.js
@@ -1,59 +1,58 @@
+// ** https://www.tiny.cloud/docs/tinymce/6/react-pm-bundle/ **//
+import React from "react";
+
 import { Editor } from "@tinymce/tinymce-react";
 
-// TinyMCE so the global var exists
-// eslint-disable-next-line no-unused-vars
-import tinymce from "tinymce/tinymce";
-// DOM model
-import "tinymce/models/dom/model";
-// Theme
-import "tinymce/themes/silver";
-// Toolbar icons
-import "tinymce/icons/default";
+if (typeof window !== "undefined") {
+  require("tinymce/tinymce");
+  require("tinymce/models/dom/model");
+  require("tinymce/themes/silver");
+  require("tinymce/icons/default");
+  require("tinymce/plugins/advlist");
+  require("tinymce/plugins/anchor");
+  require("tinymce/plugins/autolink");
+  require("tinymce/plugins/autoresize");
+  require("tinymce/plugins/autosave");
+  require("tinymce/plugins/charmap");
+  require("tinymce/plugins/code");
+  require("tinymce/plugins/codesample");
+  require("tinymce/plugins/directionality");
+  require("tinymce/plugins/emoticons");
+  require("tinymce/plugins/fullscreen");
+  require("tinymce/plugins/help");
+  require("tinymce/plugins/image");
+  require("tinymce/plugins/importcss");
+  require("tinymce/plugins/insertdatetime");
+  require("tinymce/plugins/link");
+  require("tinymce/plugins/lists");
+  require("tinymce/plugins/media");
+  require("tinymce/plugins/nonbreaking");
+  require("tinymce/plugins/pagebreak");
+  require("tinymce/plugins/preview");
+  require("tinymce/plugins/quickbars");
+  require("tinymce/plugins/save");
+  require("tinymce/plugins/searchreplace");
+  require("tinymce/plugins/table");
+  require("tinymce/plugins/template");
+  require("tinymce/plugins/visualblocks");
+  require("tinymce/plugins/visualchars");
+  require("tinymce/plugins/wordcount");
+  require("tinymce/plugins/emoticons/js/emojis");
+}
+
 // Editor styles
 import "tinymce/skins/ui/oxide/skin.min.css";
 
-// importing the plugin js.
-// if you use a plugin that is not listed here the editor will fail to load
-import "tinymce/plugins/advlist";
-import "tinymce/plugins/anchor";
-import "tinymce/plugins/autolink";
-import "tinymce/plugins/autoresize";
-import "tinymce/plugins/autosave";
-import "tinymce/plugins/charmap";
-import "tinymce/plugins/code";
-import "tinymce/plugins/codesample";
-import "tinymce/plugins/directionality";
-import "tinymce/plugins/emoticons";
-import "tinymce/plugins/fullscreen";
-import "tinymce/plugins/help";
-import "tinymce/plugins/image";
-import "tinymce/plugins/importcss";
-import "tinymce/plugins/insertdatetime";
-import "tinymce/plugins/link";
-import "tinymce/plugins/lists";
-import "tinymce/plugins/media";
-import "tinymce/plugins/nonbreaking";
-import "tinymce/plugins/pagebreak";
-import "tinymce/plugins/preview";
-import "tinymce/plugins/quickbars";
-import "tinymce/plugins/save";
-import "tinymce/plugins/searchreplace";
-import "tinymce/plugins/table";
-import "tinymce/plugins/template";
-import "tinymce/plugins/visualblocks";
-import "tinymce/plugins/visualchars";
-import "tinymce/plugins/wordcount";
-
-// importing plugin resources
-import "tinymce/plugins/emoticons/js/emojis";
-
 // Content styles, including inline UI like fake cursors
 /* eslint import/no-webpack-loader-syntax: off */
+// import contentCss from 'tinymce/skins/content/dark/content.css'
+
 import contentCss from "!!raw-loader!tinymce/skins/content/default/content.min.css";
 import contentUiCss from "!!raw-loader!tinymce/skins/ui/oxide/content.min.css";
 
 export default function BundledEditor(props) {
   const { init, ...rest } = props;
+
   // note that skin and content_css is disabled to avoid the normal
   // loading process and is instead loaded as a string via content_style
   return (
@@ -65,7 +64,7 @@ export default function BundledEditor(props) {
         content_style: [
           contentCss,
           contentUiCss,
-          init.content_style || "",
+          init?.content_style || "",
         ].join("\n"),
       }}
       {...rest}
